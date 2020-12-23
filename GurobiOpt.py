8cf921dc8bf018920f15e76f5786d9dd8d7fb034
Deraj <jae608@mail.usask.ca>
2020-12-22 21:09:29 -0600

    Added linear boundary constraints

--- GurobiOpt.py
+++ GurobiOpt.py
@@ -2,6 +2,9 @@ import gurobipy as gp
 from gurobipy import GRB
 
 #  It is assumed we can parse a csv file into an array of N rows and M columns
+table = [[25, 10, 5],
+         [5, 10, 25],
+         [10, 5, 5]]
 # table = [[2, 3, 4, 5],
 #          [4, 7, 2, 8],
 #          [8, 5, 3, 3],
@@ -12,12 +15,12 @@ from gurobipy import GRB
 #          [6, 14, 9, 4, 3],
 #          [2, 3, 11, 10, 10],
 #          [7, 3, 3, 18, 5]]
-table = [[10, 2, 12, 14, 4, 6],
-         [3, 5, 1, 15, 19, 9],
-         [8, 18, 1, 14, 17, 3],
-         [13, 4, 14, 10, 2, 3],
-         [8, 16, 20, 4, 1, 19],
-         [15, 11, 7, 3, 17, 10]]
+# table = [[10, 2, 12, 14, 4, 6],
+#          [3, 5, 1, 15, 19, 9],
+#          [8, 18, 1, 14, 17, 3],
+#          [13, 4, 14, 10, 2, 3],
+#          [8, 16, 20, 4, 1, 19],
+#          [15, 11, 7, 3, 17, 10]]
 
 
 # For an arbitrary NxM table, we would have 2NM variables, of which they are height, length, or error
@@ -29,34 +32,59 @@ def add_errs(table, model):
 
     # Lengths
     for i in range(n_row*n_col):
-        model.addVar(lb=0.0, ub=float('inf'), vtype=GRB.CONTINUOUS)
+        model.addVar(lb=0.0, ub=float('inf'), vtype=GRB.CONTINUOUS, name='l'+str(i+1))
     # Heights
     for i in range(n_row):
         model.addVar(lb=0.0, ub=float('inf'), vtype=GRB.CONTINUOUS, name='h'+str(i+1))
     # Errors
-    for i in range(n_row*(n_row*(n_col-1))):
+    for i in range(n_row*(n_col-1)):
         model.addVar(lb=0.0, ub=float('inf'), vtype=GRB.CONTINUOUS, name='e'+str(i+1))
 
 
+# Add 2(M-1)(N-1) Linear column boundary constraints
 def addL_const(table, model, eps):
     n_row = len(table)
     n_col = len(table[0])
 
     vars = model.getVars()
-    print(vars)
     lengths = vars[:len(vars)//2]
     heights = vars[len(vars)//2:len(vars)//2+n_row]
     errors = vars[len(vars)//2+n_row:]
 
-    print(lengths)
-    print(heights)
-    print(errors)
+    i = 1
 
-    # First get the column adjacency constraints, of which there are 2M*(N-1)
+    # First get the column adjacency constraints, of which there are 2(M-1)(N-1)
+    # These constraints are a+b>=c and c+d>=a
     for row in range(n_row-1):
-        for col in range(n_col):
-            pass
-            #model.addLConstr(left, sense=GRB.GREATER_EQUAL, rhs=right, name='l'+str(n))
+        for col in range(1, n_col):
+            # Initialize a,b,c,d
+            a = lengths[row*n_row:col+row*n_row]
+            a.extend(errors[row*(n_row-1):row*(n_row-1)+(col)])
+            b = lengths[col+row*n_row]
+            c = lengths[(row+1)*n_row:col+(row+1)*n_row]
+            c.extend(errors[(row+1)*(n_row-1):(row+1)*(n_row-1)+(col)])
+            d = lengths[col+(row+1)*n_row]
+
+            # Make our left and right sides for both equations
+            left1 = gp.LinExpr(b)
+            for var in a:
+                left1.add(var)
+            right1 = gp.LinExpr()
+            for var in c:
+                right1.add(var)
+            right1.add(eps)
+
+            left2 = gp.LinExpr(d)
+            for var in c:
+                left2.add(var)
+            right2 = gp.LinExpr()
+            for var in a:
+                right2.add(var)
+            right2.add(eps)
+
+            model.addLConstr(left1, sense=GRB.GREATER_EQUAL, rhs=right1, name='bc'+str(i))
+            i = i+1
+            model.addLConstr(left2, sense=GRB.GREATER_EQUAL, rhs=right2, name='bc' + str(i))
 
 try:
 
@@ -64,16 +92,17 @@ try:
 
     # Create a new model
     m = gp.Model("Solver")
-    e1 = m.addVar(lb=0.0, ub=float('inf'), vtype=GRB.CONTINUOUS, name='e1')
-    print(m)
+
     # Add the variables to our model
     add_errs(table, m)
-
-    print(m)
+    m.presolve()
 
     # Add the constraints to our model
     # Do it for both the linear and quadratic constraints separately
     addL_const(table, m, epsilon)
+    m.presolve()
+
+    print(m)
 
 except gp.GurobiError as e:
     print('Error code ' + str(e.errno) + ': ' + str(e))
